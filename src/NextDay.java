f5d6688911c69e59a7f5f68c602111f0fa9ae300
VuKieuAnh <vukieuanh.hnue@gmail.com>
2022-07-13 10:24:40 +0700

    pass test 31/1/2018

--- src/NextDay.java
+++ src/NextDay.java
@@ -3,7 +3,14 @@ public class NextDay {
     public static final String CONCATENATION = "/";
 
     public static String getNextDay(int dayTest, int monthTest, int yearTest) {
-        int day = dayTest+1;
+        int day;
+        if (dayTest==31){
+            day=1;
+            monthTest = monthTest+1;
+        }
+        else {
+            day = dayTest+1;
+        }
         String result = day+ CONCATENATION + monthTest+ CONCATENATION + yearTest;
         return  result;
     }
