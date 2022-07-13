916c3c283a29fdab402616947c4af9027481f529
VuKieuAnh <vukieuanh.hnue@gmail.com>
2022-07-13 14:49:49 +0700

    add file DemoKA

---
+++ src/DemoKA.java
@@ -0,0 +1,2 @@
+public class DemoKA {
+}
--- src/NextDayTest.java
+++ src/NextDayTest.java
@@ -46,5 +46,19 @@ class NextDayTest {
 //        A: Assert So sanh
         assertEquals(expect, result);
     }
+    @Test
+    @DisplayName("case 28/2/2018")
+    void testDay28Month2Year2018(){
+//        A: arrange Chuan bi du lieu
+        int dayTest = 28;
+        int monthTest = 2;
+        int yearTest = 2018;
+
+        String expect = "1/3/2018";
+//   ex   A: action Chay phuong thuc
+        String result = NextDay.getNextDay(dayTest, monthTest, yearTest);
+//        A: Assert So sanh
+        assertEquals(expect, result);
+    }
 
 }
